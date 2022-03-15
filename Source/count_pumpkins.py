fc978a90997349e2c1e61f6584d4758a8e16db03
PositiveBeat <nicoline@finman.dk>
2022-03-15 20:20:42 +0100

    Attemtps at better detection - not really successful

--- Source/count_pumpkins.py
+++ Source/count_pumpkins.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
-import os
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 class CountPumpkins:
@@ -23,19 +24,26 @@ class CountPumpkins:
         
     def detect(self):
         
-        img_blur = cv2.GaussianBlur(self.img_hsv, (5, 5), 0)
+        img_blur = cv2.GaussianBlur(self.img_hsv, (7, 7), 0)
         
         segmented_image = self.mahalanobis(img_blur)
+        cv2.imwrite("output/segmented.jpg", segmented_image)
         
         # Morphological filtering the image
-        kernel = np.ones((5, 5), np.uint8)
-        closed_image = cv2.morphologyEx(segmented_image, cv2.MORPH_CLOSE, kernel)        
+        kernel_cls = np.ones((11, 11), np.uint8)
+        kernel_opn = np.ones((7, 7), np.uint8)
+        morp_image = cv2.morphologyEx(segmented_image, cv2.MORPH_CLOSE, kernel_cls)
+        morp_image = cv2.morphologyEx(morp_image, cv2.MORPH_OPEN, kernel_opn)
+        cv2.imwrite("output/morp.jpg", morp_image)     
 
-        contours, hierarchy = cv2.findContours(closed_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(morp_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         
-        self.show_output(closed_image, contours)
+        # contours = self.circlepass_filter(contours)
+        contours = self.remove_outliers(contours)
         
-        
+        self.show_output(morp_image, contours)
+
+
     def mahalanobis(self, img):
         # Mahalanobis based segmentation
         shape = self.pixels.shape
@@ -48,35 +56,58 @@ class CountPumpkins:
         mahalanobis_segmented = mahalanobis_segmented.astype(np.uint8)
 
         return mahalanobis_segmented
+
+
+    def remove_outliers(self, contours):
         
+        good_contours = []
+        contour_size = []
         
+        for contour in contours:
+            size = np.sqrt(cv2.contourArea(contour))
+            if (size != 0):
+                contour_size.append(size)
+        
+        # plt.hist(contour_size, range=(0,40), bins=500)
+        # plt.show()
+        
+        for i, size in enumerate(contour_size):
+            if (size > 1):
+                good_contours.append(contours[i])
+
+        return good_contours
+
+
+    def circlepass_filter(self, contours):
+        
+        circles = []
+        thres = 0.2
+        
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            perimeter = cv2.arcLength(contour, True)
+            
+            circle_likeness = (2*np.pi * area) / (perimeter**2 + 0.00001)
+            
+            if (circle_likeness > thres):
+                circles.append(contour)
+        
+        return circles
+
+
+    
     def show_output(self, closed_image, contours):
         
         # Visual display
-        # cv2.drawContours(image=self.img, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=1, lineType=cv2.LINE_AA)
+        cv2.drawContours(image=self.img, contours=contours, contourIdx=-1, color=(0, 0, 255), thickness=1, lineType=cv2.LINE_AA)
         
-        for contour in contours:
-            M = cv2.moments(contour)
+        # for contour in contours:
+        #     M = cv2.moments(contour)
             
-            if M["m00"] != 0:
-                cx = int(M['m10'] / M['m00'])
-                cy = int(M['m01'] / M['m00'])
-                cv2.circle(self.img, (cx, cy), 8, (0, 0, 255), 2)
-
-        
-        # height, width, channels = self.img.shape 
-        # dim = (600, round(600 * height/width))
-        # img_resized = cv2.resize(self.img, dim, interpolation=cv2.INTER_LINEAR)
-        # mah_resized = cv2.resize(closed_image, dim, interpolation=cv2.INTER_LINEAR)
-
-        # cv2.namedWindow('Mahalanobis')
-        # cv2.moveWindow('Mahalanobis', 40,30)
-        # cv2.namedWindow('Contours')
-        # cv2.moveWindow('Contours', 40,500)  
-
-        # cv2.imshow('Mahalanobis', mah_resized)
-        # cv2.imshow('Contours', img_resized)
-        # cv2.waitKey(0)
+        #     if M["m00"] != 0:
+        #         cx = int(M['m10'] / M['m00'])
+        #         cy = int(M['m01'] / M['m00'])
+        #         cv2.circle(self.img, (cx, cy), 8, (0, 0, 255), 2)
 
         print("Number of detected pumpkins: %d" % len(contours))
         cv2.imwrite("output/located-objects.jpg", self.img)
@@ -85,11 +116,12 @@ class CountPumpkins:
 
 if __name__ == '__main__':
     
-    path = os.getcwd() + "/../2019-03-19 Images for third miniproject/"
+    path = "../2019-03-19 Images for third miniproject/"
     filename = path + "EB-02-660_0595_0007.JPG"
     # filename = "../Pumpkin_Field_Ortho.tif"
+    
+    # filename = "input/zoomed_original.jpg"
 
     image = cv2.imread(filename)
-
+    
     pumpkins = CountPumpkins(image)
-
